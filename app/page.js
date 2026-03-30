cdeb28fb836e831035aded722f70455481349127
Trần Tứ <thd1522005@gmail.com>
2026-03-30 17:06:26 +0700

    feat: complete product listing page with responsive grid

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default async function HomePage() {
   return (
     <div>
       <Navbar />
-      <div className="p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
         {products.map((item) => (
           <ProductCard key={item.id} product={item} />
         ))}
